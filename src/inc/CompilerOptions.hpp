19c05c54052a11956aa08c8f883cb7c676e3daed
Toby <toby.lawrance@gmail.com>
2020-10-08 17:21:36 +0100

    Added class denoting Compiler Options

---
+++ src/inc/CompilerOptions.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+enum class WARNINGS
+{
+	ALL,
+	VERBOSE,
+	INFO,
+	NONE
+};
+
+enum class OPTIMISATION
+{
+	NONE,
+	ALL,
+	EXPERIMENTAL
+};
+
+class CompilerOptions
+{
+public:
+	WARNINGS warningVerbosity = WARNINGS::NONE;
+
+	OPTIMISATION optimisationLevel = OPTIMISATION::ALL;
+
+	std::string inputFile;
+
+	std::string outputFile; //Default to name of file
+
+	CompilerOptions(const std::string& inpFile) : inputFile(inpFile), outputFile(inpFile) {}
+};
