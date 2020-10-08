6bb347671b9cafa39fdf2bfef82fcbf1ffa12ee6
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-10-08 23:05:19 +0100

    Test that the CI is actually using clang 10 by forcing use of the <=> operator.
     DO NOT MERGE ANYTHING WITH THIS COMMIT IN. REVERT ME ONCE TEST IS COMPLETE!!!

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -4,16 +4,16 @@
 
 int main(int argc, char* argv[], char* envp[]) {
     std::vector<std::string> args;
-	//First argument is always name of exe, ignore
-	for(int i = 1; i < argc; ++i)
-	{
+    //First argument is always name of exe, ignore
+    for (int i = 1; i < argc; ++i) {
         args.emplace_back(std::string(argv[i]));
-	}
+    }
 
-    std::cout << "Hello, World!" << std::endl;
-    for (auto arg : args)
-    {
-        std::cout << arg << " argument applied" << std::endl ;
+    if (2<=>3 == std::strong_ordering::less)
+        std::cout << "Hello, World!" << std::endl;
+
+    for (auto arg : args) {
+        std::cout << arg << " argument applied" << std::endl;
     }
     return 0;
 }
