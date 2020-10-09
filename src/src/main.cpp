d6d3f41bc786e26804fceb4b86630c3d2bc2668e
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-10-09 01:26:37 +0100

    Add clang-format with a .clang-format file to ensure consistent styling. Using google as a starting point. Can change this if people have strong opinions.

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 int main(int argc, char* argv[], char* envp[]) {
-    //First argument is always name of exe, ignore
-	std::vector<std::string> args(argv+1, argv+argc);
+    // First argument is always name of exe, ignore
+    std::vector<std::string> args(argv + 1, argv + argc);
 
-    //if (2<=>3 == std::strong_ordering::less)
+    // if (2<=>3 == std::strong_ordering::less)
     //    std::cout << "Hello, World!" << std::endl;
 
-	for (auto arg : args) {
+    for (auto arg : args) {
         std::cout << arg << " argument applied" << std::endl;
     }
     return 0;
