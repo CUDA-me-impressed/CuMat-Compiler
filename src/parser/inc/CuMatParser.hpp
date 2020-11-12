97d077bf7cac3b9d4b414b8102c4bc50f3a34042
Toby <toby.lawrance@gmail.com>
2020-11-12 14:54:49 +0000

    Time to add Parsing

---
+++ src/parser/inc/CuMatParser.hpp
@@ -0,0 +1,13 @@
+//
+// Created by tobyl on 12/11/2020.
+//
+
+#ifndef _CUMATPARSER_HPP_
+#define _CUMATPARSER_HPP_
+
+class CuMatParser
+{
+
+};
+
+#endif  //_CUMATPARSER_HPP_
---
+++ src/parser/src/CuMatParser.cpp
@@ -0,0 +1,3 @@
+//
+// Created by tobyl on 12/11/2020.
+//
