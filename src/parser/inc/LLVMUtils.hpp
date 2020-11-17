7d4dc0642ce771ba57cfdf1afcaa554e3215388c
Alfie <powersalfie@gmail.com>
2020-11-17 19:21:17 +0000

    Really clang?

--- src/parser/inc/LLVMUtils.hpp
+++ src/parser/inc/LLVMUtils.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <llvm-10/llvm/IR/Module.h>
-#include <llvm-10/llvm/IR/LLVMContext.h>
 #include <llvm-10/llvm/ADT/StringRef.h>
+#include <llvm-10/llvm/IR/LLVMContext.h>
+#include <llvm-10/llvm/IR/Module.h>
+
 #include <memory>
 
 struct LLVMUtils {
