11b3339e57a8679ba2ccf37d86d31ec6b5699cd4
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-11-18 16:33:44 +0000

    Update pointer styles in extant code to reflect pointer style

--- src/parser/inc/CuMatVisitor.hpp
+++ src/parser/inc/CuMatVisitor.hpp
@@ -11,32 +11,32 @@
 
 class CuMatVisitor : public CuMatParserBaseVisitor {
    public:
-    antlrcpp::Any visitProgram(CuMatParser::ProgramContext *ctx) override;
+    antlrcpp::Any visitProgram(CuMatParser::ProgramContext* ctx) override;
 
    private:
-    antlrcpp::Any visitImports(CuMatParser::ImportsContext *ctx) override;
+    antlrcpp::Any visitImports(CuMatParser::ImportsContext* ctx) override;
 
     antlrcpp::Any visitDefinitions(
-        CuMatParser::DefinitionsContext *ctx) override;
-    antlrcpp::Any visitDefinition(CuMatParser::DefinitionContext *ctx) override;
+        CuMatParser::DefinitionsContext* ctx) override;
+    antlrcpp::Any visitDefinition(CuMatParser::DefinitionContext* ctx) override;
 
-    antlrcpp::Any visitFuncdef(CuMatParser::FuncdefContext *ctx) override;
+    antlrcpp::Any visitFuncdef(CuMatParser::FuncdefContext* ctx) override;
 
-    antlrcpp::Any visitAssignment(CuMatParser::AssignmentContext *ctx) override;
-    antlrcpp::Any visitVarname(CuMatParser::VarnameContext *ctx) override;
+    antlrcpp::Any visitAssignment(CuMatParser::AssignmentContext* ctx) override;
+    antlrcpp::Any visitVarname(CuMatParser::VarnameContext* ctx) override;
 
-    antlrcpp::Any visitExpression(CuMatParser::ExpressionContext *ctx) override;
-    antlrcpp::Any visitExp_logic(CuMatParser::Exp_logicContext *ctx) override;
-    antlrcpp::Any visitExp_comp(CuMatParser::Exp_compContext *ctx) override;
-    antlrcpp::Any visitExp_bit(CuMatParser::Exp_bitContext *ctx) override;
-    antlrcpp::Any visitExp_sum(CuMatParser::Exp_sumContext *ctx) override;
-    antlrcpp::Any visitExp_mult(CuMatParser::Exp_multContext *ctx) override;
-    antlrcpp::Any visitExp_pow(CuMatParser::Exp_powContext *ctx) override;
-    antlrcpp::Any visitExp_mat(CuMatParser::Exp_matContext *ctx) override;
-    antlrcpp::Any visitExp_neg(CuMatParser::Exp_negContext *ctx) override;
-    antlrcpp::Any visitExp_not(CuMatParser::Exp_notContext *ctx) override;
-    antlrcpp::Any visitExp_chain(CuMatParser::Exp_chainContext *ctx) override;
-    antlrcpp::Any visitExp_func(CuMatParser::Exp_funcContext *ctx) override;
+    antlrcpp::Any visitExpression(CuMatParser::ExpressionContext* ctx) override;
+    antlrcpp::Any visitExp_logic(CuMatParser::Exp_logicContext* ctx) override;
+    antlrcpp::Any visitExp_comp(CuMatParser::Exp_compContext* ctx) override;
+    antlrcpp::Any visitExp_bit(CuMatParser::Exp_bitContext* ctx) override;
+    antlrcpp::Any visitExp_sum(CuMatParser::Exp_sumContext* ctx) override;
+    antlrcpp::Any visitExp_mult(CuMatParser::Exp_multContext* ctx) override;
+    antlrcpp::Any visitExp_pow(CuMatParser::Exp_powContext* ctx) override;
+    antlrcpp::Any visitExp_mat(CuMatParser::Exp_matContext* ctx) override;
+    antlrcpp::Any visitExp_neg(CuMatParser::Exp_negContext* ctx) override;
+    antlrcpp::Any visitExp_not(CuMatParser::Exp_notContext* ctx) override;
+    antlrcpp::Any visitExp_chain(CuMatParser::Exp_chainContext* ctx) override;
+    antlrcpp::Any visitExp_func(CuMatParser::Exp_funcContext* ctx) override;
 };
 
 #endif  //_CUMATPARSER_HPP_
--- src/parser/inc/parser.hpp
+++ src/parser/inc/parser.hpp
@@ -10,9 +10,9 @@
 #include "ASTNode.hpp"
 
 class SimpleErrorListener : public antlr4::BaseErrorListener {
-    void syntaxError(antlr4::Recognizer *recognizer,
-                     antlr4::Token *offendingSymbol, size_t line,
-                     size_t charPositionInLine, const std::string &msg,
+    void syntaxError(antlr4::Recognizer* recognizer,
+                     antlr4::Token* offendingSymbol, size_t line,
+                     size_t charPositionInLine, const std::string& msg,
                      std::exception_ptr e) override;
 };
 
