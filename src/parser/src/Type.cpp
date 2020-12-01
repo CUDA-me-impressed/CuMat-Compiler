557dbe82dd7636540eee628d15ae789e32e45273
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-12-01 15:26:32 +0000

    Change typing structure

--- src/parser/src/Type.cpp
+++ src/parser/src/Type.cpp
@@ -1,5 +1,8 @@
 #include "Type.hpp"
 
+#include <llvm/IR/Module.h>
+
+#include <numeric>
 #include <stdexcept>
 
 /**
@@ -7,7 +10,7 @@
  * primitive type within CuMat
  * @return
  */
-int Typing::Type::offset() {
+int Typing::MatrixType::offset() {
     switch (primType) {
         case PRIMITIVE::STRING:
         case PRIMITIVE::BOOL:
@@ -18,4 +21,40 @@ int Typing::Type::offset() {
         case PRIMITIVE::NONE:
             throw std::runtime_error("Invalid type for offset");
     }
+}
+int Typing::MatrixType::getLength() {
+    return std::accumulate(this->dimensions.begin(), this->dimensions.end(), 1,
+                           std::multiplies());
+}
+llvm::Type* Typing::MatrixType::getLLVMType(llvm::Module* module) {
+    llvm::Type* ty;
+    switch (this->primType) {
+        case Typing::PRIMITIVE::INT: {
+            ty = static_cast<llvm::Type*>(
+                llvm::Type::getInt64Ty(module->getContext()));
+            break;
+        }
+        case Typing::PRIMITIVE::FLOAT: {
+            ty = llvm::Type::getFloatTy(module->getContext());
+            break;
+        }
+        case Typing::PRIMITIVE::BOOL: {
+            ty = static_cast<llvm::Type*>(
+                llvm::Type::getInt1Ty(module->getContext()));
+            break;
+        }
+        default: {
+            std::cerr << "Cannot find a valid type for " << this->literalText
+                      << std::endl;
+            // Assign the type to be an integer
+            ty = static_cast<llvm::Type*>(
+                llvm::Type::getInt64Ty(module->getContext()));
+            break;
+        }
+        case Typing::PRIMITIVE::STRING:
+            break;
+        case Typing::PRIMITIVE::NONE:
+            break;
+    }
+    return ty;
 }
