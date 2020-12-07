31ecd7f5f65f628fdf6819788b80d099db3997f5
Toby <toby.lawrance@gmail.com>
2020-12-07 17:52:38 +0000

    Removed unnecessary interface items

--- src/parser/inc/MatrixNode.hpp
+++ src/parser/inc/MatrixNode.hpp
@@ -14,10 +14,8 @@ class MatrixNode : public ExprNode {
    public:
     std::vector<std::vector<std::shared_ptr<ExprNode>>> data;
 
-    size_t numElements();
     std::vector<int> getDimensions();
     llvm::APInt genAPIntInstance(int numElements);
-    llvm::Type* getLLVMType(llvm::Module* module);
     llvm::APFloat genAPFloatInstance(int numElements);
     llvm::Value* codeGen(Utils::IRContext* context) override;
 };
