a3365d4e269b86ca2d9e8883f7129de9b7bfadf2
Alfie <powersalfie@gmail.com>
2020-12-29 21:01:50 +0000

    Return values correct

--- src/parser/src/UnaryExprNode.cpp
+++ src/parser/src/UnaryExprNode.cpp
@@ -1,17 +1,24 @@
 #include "UnaryExprNode.hpp"
 
 #include "CodeGenUtils.hpp"
-#include "MatrixNode.hpp"
 
+/**
+ * Code generation for unary elements.
+ * We only consider each element as a single value and apply the operation as a function
+ * This is based off the Binary operation code with the subtraction of the right hand side
+ * of the expression being required, as LLVM handles this for us.
+ * @param context
+ * @return pointer to the resultant matrix initial position of the result
+ */
 llvm::Value* AST::UnaryExprNode::codeGen(Utils::IRContext* context) {
     // We go through and apply the relevant unary operator to each element of
     // the matrix
     llvm::Value* operand = this->operand->codeGen(context);
 
     auto operandMatNode = std::dynamic_pointer_cast<AST::ExprNode>(this->operand);
-
+    llvm::Value* matAlloc;
     if (auto* operandType = std::get_if<Typing::MatrixType>(&*operandMatNode->type)) {
-        auto newMatAlloc = Utils::createMatrix(context, *operandType);
+        matAlloc = Utils::createMatrix(context, *operandType);
 
         // We generate the operations sequentially
         // TODO: Add Kernel call for nvptx
@@ -24,7 +31,7 @@ llvm::Value* AST::UnaryExprNode::codeGen(Utils::IRContext* context) {
 
         auto indexAlloca = Utils::CreateEntryBlockAlloca(*Builder, "", llvm::Type::getInt64Ty(Builder->getContext()));
         auto* matSize = Utils::getLength(context, operand, *operandType);
-        auto* nsize = Utils::getLength(context, newMatAlloc, *operandType);
+        auto* nsize = Utils::getLength(context, matAlloc, *operandType);
         // parent->getBasicBlockList().push_back(addBB);
         Builder->CreateBr(addBB);
 
@@ -34,6 +41,7 @@ llvm::Value* AST::UnaryExprNode::codeGen(Utils::IRContext* context) {
 
             auto* lindex = Builder->CreateURem(index, matSize);
             auto* v = Utils::getValueFromMatrixPtr(context, operand, lindex, "lhs");
+
             llvm::Value* opResult;
             switch (op) {
                 case NEG: {
@@ -44,7 +52,7 @@ llvm::Value* AST::UnaryExprNode::codeGen(Utils::IRContext* context) {
                     opResult = context->Builder->CreateNot(v, UNA_OP_ENUM_STRING[op]);
                 }
             }
-            Utils::setValueFromMatrixPtr(context, newMatAlloc, index, opResult);
+            Utils::setValueFromMatrixPtr(context, matAlloc, index, opResult);
 
             // Update counter
             auto* next = Builder->CreateAdd(
@@ -59,5 +67,5 @@ llvm::Value* AST::UnaryExprNode::codeGen(Utils::IRContext* context) {
         parent->getBasicBlockList().push_back(endBB);
         Builder->SetInsertPoint(endBB);
     }
-    return nullptr;
+    return matAlloc;
 }
