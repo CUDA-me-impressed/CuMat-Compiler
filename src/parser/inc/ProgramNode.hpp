e633b3065990c505430bc9ee97627a609cd76afb
Toby <toby.lawrance@gmail.com>
2021-01-17 21:28:32 +0000

    Inclusion of additional nodes
    ProgramNode,SliceNode, adjustment of VariableNode to include SliceNode and Namespacing

---
+++ src/parser/inc/ProgramNode.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ASTNode.hpp"
+
+namespace AST {
+class ProgramNode : public Node {
+   public:
+    llvm::Value* codeGen(Utils::IRContext* context) override;
+};
+}  // namespace AST
--- src/parser/inc/VariableNode.hpp
+++ src/parser/inc/VariableNode.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <vector>
+
 #include "ExprASTNode.hpp"
+#include "SliceNode.hpp"
 
 namespace AST {
 class VariableNode : public ExprNode {
    public:
+    std::vector<std::string> namespacePath;
     std::string name;
-    llvm::Value* codeGen(Utils::IRContext* context);
+    std::shared_ptr<SliceNode> variableSlicing;
+    llvm::Value* codeGen(Utils::IRContext* context) override;
 };
 }  // namespace AST
