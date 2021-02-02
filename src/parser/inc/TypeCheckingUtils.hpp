2d76d7242938b024e08a49167b89562351495828
Lloyd Conlin <lloyd.conlin@gmail.com>
2021-02-02 14:55:40 +0000

    Added new type checking utility functions

--- src/parser/inc/TypeCheckingUtils.hpp
+++ src/parser/inc/TypeCheckingUtils.hpp
@@ -9,4 +9,16 @@
 
 #include "Type.hpp"
 
-std::shared_ptr<Typing::Type> makeGenericType(std::string typeString);
+int wrongTypeCode = 2;
+int typeMismatchCode = 3;
+
+std::shared_ptr<Typing::Type> makeMatrixType(const std::vector<uint> dimensions, Typing::PRIMITIVE primType);
+
+void assertLogicalType(Typing::PRIMITIVE ty);
+void assertNumericType(Typing::PRIMITIVE ty);
+void assertBooleanType(Typing::PRIMITIVE ty);
+void assertMatchingTypes(Typing::PRIMITIVE lhs, Typing::PRIMITIVE rhs);
+
+std::string primToString(Typing::PRIMITIVE ty);
+
+void wrongTypeError(std::string message, Typing::PRIMITIVE ty);
--- src/parser/src/TypeCheckingUtils.cpp
+++ src/parser/src/TypeCheckingUtils.cpp
@@ -4,20 +4,64 @@
 
 #include "TypeCheckingUtils.hpp"
 
+#include <iostream>
+
 #include "Type.hpp"
 
-std::shared_ptr<Typing::Type> makeGenericType(std::string typeString) {
-    auto ty = Typing::GenericType();
-    ty.name = typeString;
+std::shared_ptr<Typing::Type> makeMatrixType(const std::vector<uint> dimensions, Typing::PRIMITIVE primType) {
+    auto ty = Typing::MatrixType();
+    ty.dimensions = dimensions;
+    ty.rank = dimensions.size();
+    ty.primType = primType;
     std::shared_ptr<Typing::Type> type = std::make_shared<Typing::Type>(ty);
     return type;
 }
 
-std::shared_ptr<Typing::Type> makeMatrixType(const std::vector<uint> dimensions) {
-    auto ty = Typing::MatrixType();
-    ty.dimensions = dimensions;
-    ty.rank = dimensions.size();
-    //    ty.primType = ;
-    std::shared_ptr<Typing::Type> type = std::make_shared<Typing::Type>(ty);
-    return type;
+void assertLogicalType(Typing::PRIMITIVE ty) {
+    if (not(ty == Typing::PRIMITIVE::BOOL or ty == Typing::PRIMITIVE::INT)) {
+        std::string message = "Expected: bool, int";
+        wrongTypeError(message, ty);
+    }
+}
+
+void assertNumericType(Typing::PRIMITIVE ty) {
+    if (not(ty == Typing::PRIMITIVE::INT or ty == Typing::PRIMITIVE::FLOAT)) {
+        std::string message = "Expected: int, float";
+        wrongTypeError(message, ty);
+    }
+}
+
+void assertBooleanType(Typing::PRIMITIVE ty) {
+    if (not(ty == Typing::PRIMITIVE::BOOL)) {
+        std::string message = "Expected: bool";
+        wrongTypeError(message, ty);
+    }
+}
+
+std::string primToString(Typing::PRIMITIVE ty) {
+    switch (ty) {
+        case Typing::PRIMITIVE::STRING:
+            return "string";
+        case Typing::PRIMITIVE::INT:
+            return "int";
+        case Typing::PRIMITIVE::FLOAT:
+            return "float";
+        case Typing::PRIMITIVE::BOOL:
+            return "bool";
+        case Typing::PRIMITIVE::NONE:
+            return "none";
+    }
+}
+
+void wrongTypeError(std::string message, Typing::PRIMITIVE ty) {
+    std::cerr << "Wrong type encountered/n" << message << "/n"
+              << "Found: " << primToString(ty) << std::endl;
+    std::exit(wrongTypeCode);
+}
+
+void assertMatchingTypes(Typing::PRIMITIVE lhs, Typing::PRIMITIVE rhs) {
+    if (not(lhs == rhs)) {
+        std::cerr << "Mismatched types: " << primToString(lhs) << ", " << primToString(rhs) << std::endl;
+        std::exit(typeMismatchCode);
+    }
 }
