ac3e7d6e8b64fec818510b1123837f26c69460dd
Alfie <powersalfie@gmail.com>
2021-02-08 12:25:16 +0000

    Bringing multiplication to speed

--- cudaBLAS/mult/mult.cu
+++ cudaBLAS/mult/mult.cu
@@ -14,93 +14,18 @@
 
 __global__ void CuMatMatMultKernel(const long *matA, const long *matB, long* matRes, int width, int i, int j)
 {
+    // Get out the indicies for the multiplication
     int r = blockIdx.y * blockDim.y + threadIdx.y;
     int c = blockIdx.x * blockDim.x + threadIdx.x;
-    // check boundry conditions
+    // check boundry conditions incase block size is at the end
     if( r < i && c < j){
-        // do the multiplication for one row and col
         long value = 0;
         for(int k = 0; k < width; k++){
             value += matA[r * width + k] * matB[k * j + c];
         }
-        // store the result
         matRes[r * j + c] = value;
     }
 }
-
-/*
-template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(long *C, long *A, long *B, int wA, int wB) {
-    // Block index
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-
-    // Thread index
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-
-    // Index of the first sub-matrix of A processed by the block
-    int aBegin = wA * BLOCK_SIZE * by;
-
-    // Index of the last sub-matrix of A processed by the block
-    int aEnd   = aBegin + wA - 1;
-
-    // Step size used to iterate through the sub-matrices of A
-    int aStep  = BLOCK_SIZE;
-
-    // Index of the first sub-matrix of B processed by the block
-    int bBegin = BLOCK_SIZE * bx;
-
-    // Step size used to iterate through the sub-matrices of B
-    int bStep  = BLOCK_SIZE * wB;
-
-    // Csub is used to store the element of the block sub-matrix
-    // that is computed by the thread
-    long Csub = 0;
-
-    // Loop over all the sub-matrices of A and B
-    // required to compute the block sub-matrix
-    for (int a = aBegin, b = bBegin;
-         a <= aEnd;
-         a += aStep, b += bStep) {
-        // Declaration of the shared memory array As used to
-        // store the sub-matrix of A
-        __shared__ long As[BLOCK_SIZE][BLOCK_SIZE];
-
-        // Declaration of the shared memory array Bs used to
-        // store the sub-matrix of B
-        __shared__ long Bs[BLOCK_SIZE][BLOCK_SIZE];
-
-        // Load the matrices from device memory
-        // to shared memory; each thread loads
-        // one element of each matrix
-        As[ty][tx] = A[a + wA * ty + tx];
-        Bs[ty][tx] = B[b + wB * ty + tx];
-
-        // Synchronize to make sure the matrices are loaded
-        __syncthreads();
-
-        // Multiply the two matrices together;
-        // each thread computes one element
-        // of the block sub-matrix
-#pragma unroll
-
-        for (int k = 0; k < BLOCK_SIZE; ++k) {
-            Csub += As[ty][k] * Bs[k][tx];
-        }
-
-        // Synchronize to make sure that the preceding
-        // computation is done before loading two new
-        // sub-matrices of A and B in the next iteration
-        __syncthreads();
-    }
-
-    // Write the block sub-matrix to device memory;
-    // each thread writes one element
-    int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-    C[c + wB * ty + tx] = Csub;
-}
-
-*/
 
 void CuMatMultMatrixI(long * matA, long * matB, long * matRes, long i, long p, long j){
     auto matASize = sizeof(long) * i * p;
