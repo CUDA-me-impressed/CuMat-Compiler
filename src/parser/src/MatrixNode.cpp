7242f6d73d8224b03478096db7c2ee89f3ec294f
Alfie <powersalfie@gmail.com>
2021-02-10 15:35:38 +0000

    Intermediate probably broken

--- src/parser/src/MatrixNode.cpp
+++ src/parser/src/MatrixNode.cpp
@@ -13,37 +13,39 @@
 #include "TypeCheckingUtils.hpp"
 
 llvm::Value* AST::MatrixNode::codeGen(Utils::IRContext* context) {
-    // Get the LLVM type out for the basic type
+//    // Get the LLVM type out for the basic type
+//    Typing::MatrixType matTypeAST = std::get<Typing::MatrixType>(*this->type);
+//    llvm::Type* ty = matTypeAST.getLLVMType(context);
+//    // Get function to store this data within
+//    llvm::ArrayType* matType = llvm::ArrayType::get(ty, matTypeAST.getLength());
+//
+//    // Create a store instance for the correct precision and data type
+//    // Address space set to zero
+//    auto matAlloc = context->Builder->CreateAlloca(matType, 0, nullptr, "matVar");
+//
+//    // We need to fill in the data for each of the elements of the array:
+//    std::vector<llvm::Value*> matElements(matTypeAST.getLength());
+//    auto zero = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, 0, true));
+//    for (int row = 0; row < data.size(); row++) {
+//        for (int column = 0; column < data[row].size(); column++) {
+//            // Generate the code for the element -> The Value* will be what
+//            // we store within the matrix location so depending on what we are
+//            // storing, it must be sufficient to run
+//            size_t elIndex = row * data.size() + column;
+//            llvm::Value* val = data[row][column]->codeGen(context);
+//
+//            // Create index for current index of the value
+//            auto index = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, elIndex, true));
+//
+//            // Get pointer to the index location within memory
+//            auto ptr = llvm::GetElementPtrInst::Create(matType, matAlloc, {zero, index}, "",
+//                                                       context->Builder->GetInsertBlock());
+//            context->Builder->CreateStore(val, ptr);
+//        }
+//    }
+//    return matAlloc;
+    // Assume every expr is an evaluated matrix
     Typing::MatrixType matTypeAST = std::get<Typing::MatrixType>(*this->type);
-    llvm::Type* ty = matTypeAST.getLLVMType(context);
-    // Get function to store this data within
-    llvm::ArrayType* matType = llvm::ArrayType::get(ty, matTypeAST.getLength());
-
-    // Create a store instance for the correct precision and data type
-    // Address space set to zero
-    auto matAlloc = context->Builder->CreateAlloca(matType, 0, nullptr, "matVar");
-
-    // We need to fill in the data for each of the elements of the array:
-    std::vector<llvm::Value*> matElements(matTypeAST.getLength());
-    auto zero = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, 0, true));
-    for (int row = 0; row < data.size(); row++) {
-        for (int column = 0; column < data[row].size(); column++) {
-            // Generate the code for the element -> The Value* will be what
-            // we store within the matrix location so depending on what we are
-            // storing, it must be sufficient to run
-            size_t elIndex = row * data.size() + column;
-            llvm::Value* val = data[row][column]->codeGen(context);
-
-            // Create index for current index of the value
-            auto index = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, elIndex, true));
-
-            // Get pointer to the index location within memory
-            auto ptr = llvm::GetElementPtrInst::Create(matType, matAlloc, {zero, index}, "",
-                                                       context->Builder->GetInsertBlock());
-            context->Builder->CreateStore(val, ptr);
-        }
-    }
-    return matAlloc;
 }
 
 llvm::APInt AST::MatrixNode::genAPIntInstance(const int numElements) {
