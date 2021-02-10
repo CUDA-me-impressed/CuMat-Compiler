d530a85172b8ad4b39f86a04f8ecb9274efe851c
Alfie <powersalfie@gmail.com>
2021-02-10 16:00:26 +0000

    Matrices should actually work now
    Literals are converted, will add in memory cleanup

--- src/parser/src/MatrixNode.cpp
+++ src/parser/src/MatrixNode.cpp
@@ -13,48 +13,50 @@
 #include "TypeCheckingUtils.hpp"
 
 llvm::Value* AST::MatrixNode::codeGen(Utils::IRContext* context) {
-//    // Get the LLVM type out for the basic type
-//    Typing::MatrixType matTypeAST = std::get<Typing::MatrixType>(*this->type);
-//    llvm::Type* ty = matTypeAST.getLLVMType(context);
-//    // Get function to store this data within
-//    llvm::ArrayType* matType = llvm::ArrayType::get(ty, matTypeAST.getLength());
-//
-//    // Create a store instance for the correct precision and data type
-//    // Address space set to zero
-//    auto matAlloc = context->Builder->CreateAlloca(matType, 0, nullptr, "matVar");
-//
-//    // We need to fill in the data for each of the elements of the array:
-//    std::vector<llvm::Value*> matElements(matTypeAST.getLength());
-//    auto zero = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, 0, true));
-//    for (int row = 0; row < data.size(); row++) {
-//        for (int column = 0; column < data[row].size(); column++) {
-//            // Generate the code for the element -> The Value* will be what
-//            // we store within the matrix location so depending on what we are
-//            // storing, it must be sufficient to run
-//            size_t elIndex = row * data.size() + column;
-//            llvm::Value* val = data[row][column]->codeGen(context);
-//
-//            // Create index for current index of the value
-//            auto index = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, elIndex, true));
-//
-//            // Get pointer to the index location within memory
-//            auto ptr = llvm::GetElementPtrInst::Create(matType, matAlloc, {zero, index}, "",
-//                                                       context->Builder->GetInsertBlock());
-//            context->Builder->CreateStore(val, ptr);
-//        }
-//    }
-//    return matAlloc;
+    //    // Get the LLVM type out for the basic type
+    //    Typing::MatrixType matTypeAST = std::get<Typing::MatrixType>(*this->type);
+    //    llvm::Type* ty = matTypeAST.getLLVMType(context);
+    //    // Get function to store this data within
+    //    llvm::ArrayType* matType = llvm::ArrayType::get(ty, matTypeAST.getLength());
+    //
+    //    // Create a store instance for the correct precision and data type
+    //    // Address space set to zero
+    //    auto matAlloc = context->Builder->CreateAlloca(matType, 0, nullptr, "matVar");
+    //
+    //    // We need to fill in the data for each of the elements of the array:
+    //    std::vector<llvm::Value*> matElements(matTypeAST.getLength());
+    //    auto zero = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, 0, true));
+    //    for (int row = 0; row < data.size(); row++) {
+    //        for (int column = 0; column < data[row].size(); column++) {
+    //            // Generate the code for the element -> The Value* will be what
+    //            // we store within the matrix location so depending on what we are
+    //            // storing, it must be sufficient to run
+    //            size_t elIndex = row * data.size() + column;
+    //            llvm::Value* val = data[row][column]->codeGen(context);
+    //
+    //            // Create index for current index of the value
+    //            auto index = llvm::ConstantInt::get(context->module->getContext(), llvm::APInt(64, elIndex, true));
+    //
+    //            // Get pointer to the index location within memory
+    //            auto ptr = llvm::GetElementPtrInst::Create(matType, matAlloc, {zero, index}, "",
+    //                                                       context->Builder->GetInsertBlock());
+    //            context->Builder->CreateStore(val, ptr);
+    //        }
+    //    }
+    //    return matAlloc;
     // Assume every expr is an evaluated matrix
     auto matAlloc = Utils::createMatrix(context, *this->type);
     auto matRecord = Utils::getMatrixFromPointer(context, matAlloc);
     // Compress literal nodes to matrix representation
-    for(int i = 0; i < this->data.size(); i++){
+    for (int i = 0; i < this->data.size(); i++) {
         auto literal = this->data.at(i);
-        llvm::Value* literalLLVMVal = literal->codeGen(context);
-        auto literalRecord = Utils::getMatrixFromPointer(context, literalLLVMVal);
-        Utils::insertValueAtPointerOffset(context, matRecord.dataPtr, )
-
+        llvm::Value* literalLLVMMat = literal->codeGen(context);
+        auto literalRecord = Utils::getMatrixFromPointer(context, literalLLVMMat);
+        // Get the value at the pointer
+        llvm::Value* literalLLVVal = Utils::getValueFromPointerOffset(context, literalRecord.dataPtr, 0, "literalVal");
+        Utils::insertValueAtPointerOffset(context, matRecord.dataPtr, 0, literalLLVVal);
     }
+    return matAlloc;
 }
 
 llvm::APInt AST::MatrixNode::genAPIntInstance(const int numElements) {
@@ -73,8 +75,8 @@ llvm::APInt AST::MatrixNode::genAPIntInstance(const int numElements) {
  * @return
  */
 std::vector<uint> AST::MatrixNode::getDimensions() {
-    // TODO: Fix with Thomas's dimension change
-    return std::vector<uint>({static_cast<uint>(data.size()), static_cast<uint>(data[0].size())});
+    auto matType = std::get_if<Typing::MatrixType>(&*type);
+    return matType ? matType->dimensions : std::vector<uint>();
 }
 
 void AST::MatrixNode::semanticPass() {
@@ -85,27 +87,25 @@ void AST::MatrixNode::semanticPass() {
     Typing::PRIMITIVE primType = Typing::PRIMITIVE::NONE;
     Typing::MatrixType exprType;
     // Runs through all elements to check types and ranks
-    for (auto& row : data) {
-        for (auto& el : row) {
-            try {
-                exprType = std::get<Typing::MatrixType>(*el->type);
-            } catch (std::bad_cast b) {
-                std::cout << "Caught: " << b.what();
-            }
-            Typing::PRIMITIVE prim = exprType.getPrimitiveType();
-            if (primType == Typing::PRIMITIVE::NONE && prim != Typing::PRIMITIVE::NONE) {
-                primType = prim;
-            } else {
-                sameType = sameType && (primType == prim);
-            }
-            zeroRank = zeroRank && (exprType.rank == 0);
-            if (!(sameType)) {
-                std::cout << "All elements of matrix must be of same type";
-                std::exit(2);
-            } else if (!(zeroRank)) {
-                std::cout << "All elements of matrix must be scalars";
-                std::exit(2);
-            }
+    for (auto& el : data) {
+        try {
+            exprType = std::get<Typing::MatrixType>(*el->type);
+        } catch (std::bad_cast b) {
+            std::cout << "Caught: " << b.what();
+        }
+        Typing::PRIMITIVE prim = exprType.getPrimitiveType();
+        if (primType == Typing::PRIMITIVE::NONE && prim != Typing::PRIMITIVE::NONE) {
+            primType = prim;
+        } else {
+            sameType = sameType && (primType == prim);
+        }
+        zeroRank = zeroRank && (exprType.rank == 0);
+        if (!(sameType)) {
+            std::cout << "All elements of matrix must be of same type";
+            std::exit(2);
+        } else if (!(zeroRank)) {
+            std::cout << "All elements of matrix must be scalars";
+            std::exit(2);
         }
     }
 
