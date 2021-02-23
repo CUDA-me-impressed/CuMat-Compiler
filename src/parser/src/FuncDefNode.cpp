39f1a81252a8211c51669f523307a618176f3b88
Alfie <powersalfie@gmail.com>
2021-02-23 20:05:58 +0000

    Symbol table function stack codegen

--- src/parser/src/FuncDefNode.cpp
+++ src/parser/src/FuncDefNode.cpp
@@ -9,6 +9,8 @@ llvm::Value* AST::FuncDefNode::codeGen(Utils::IRContext* context) {
         argTypes.push_back(std::get<Typing::MatrixType>(*typeNamePair.second).getLLVMType(context));
     }
 
+    context->symbolTable->enterFunction(funcName);
+
     // Get out the type and create a function
     auto mt = std::get<Typing::MatrixType>(*this->returnType);
     // TODO: Figure out if we should be passing a pointer or the whole struct
