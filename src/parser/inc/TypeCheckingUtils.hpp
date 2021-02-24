58c44ef30aca139e10610217edab305560c03553
Lloyd Conlin <lloyd.conlin@gmail.com>
2021-02-24 14:42:43 +0000

    Added new error codes and functions

--- src/parser/inc/TypeCheckingUtils.hpp
+++ src/parser/inc/TypeCheckingUtils.hpp
@@ -7,10 +7,16 @@
 namespace TypeCheckUtils {
 
     enum ErrorCodes {
-        WRONG_TYPE = 2, MISMATCH_CODE = 3
+        WRONG_TYPE = 2, MISMATCH_CODE = 3, CASTING_ERROR = 4, NONE_ERROR = 5
     };
 
     std::shared_ptr<Typing::Type> makeMatrixType(const std::vector<uint> dimensions, Typing::PRIMITIVE primType);
+
+    bool isBool(Typing::PRIMITIVE ty);
+    bool isInt(Typing::PRIMITIVE ty);
+    bool isFloat(Typing::PRIMITIVE ty);
+    bool isString(Typing::PRIMITIVE ty);
+    bool isNone(Typing::PRIMITIVE ty);
 
     void assertLogicalType(Typing::PRIMITIVE ty);
     void assertNumericType(Typing::PRIMITIVE ty);
@@ -21,8 +27,10 @@ namespace TypeCheckUtils {
     std::string primToString(Typing::PRIMITIVE ty);
 
     void wrongTypeError(std::string message, Typing::PRIMITIVE ty);
+    void castingError();
 
     std::string primToString(Typing::PRIMITIVE ty);
 
     Typing::MatrixType extractMatrixType(std::shared_ptr<AST::ExprNode> node);
+    Typing::PRIMITIVE getHighestType(Typing::PRIMITIVE lhs, Typing::PRIMITIVE rhs);
 }
