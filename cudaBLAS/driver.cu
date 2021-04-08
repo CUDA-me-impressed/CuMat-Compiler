3095e9b38ff305168873a80d10302271b8a0e047
tobyl <toby.lawrance@gmail.com>
2021-04-08 15:24:09 +0100

    nvcc driver.cu -lcublas

--- cudaBLAS/driver.cu
+++ cudaBLAS/driver.cu
@@ -1,14 +1,13 @@
-#include "addition/add.cu"
-#include "mult/mult.cu"
+#include "elementwise/add.cu"
 #include <iostream>
 
 int main(){
     long A[6] = {1,2,7,3,4,1};
     long B[6] = {4,3,8,2,1,3};
-    long C[9];
+    long C[6];
 
-    CuMatMultMatrixI(A,B,C,3,2,3);
-    for(int i = 0; i < 9; i++){
+    CuMatAddMatrixI(A,B,C,6,6);
+    for(int i = 0; i < 6; i++){
         std::cout << C[i] << std::endl;
     }
 }
