da7d2ce0edfa74fbb8d617a03fde462e8dfe6113
Alfie <powersalfie@gmail.com>
2021-04-08 15:34:06 +0100

    Change

--- cudaBLAS/driver.cu
+++ cudaBLAS/driver.cu
@@ -8,24 +8,8 @@ int main(){
     long B[6] = {4,3,8,2,1,3};
     long C[9];
 
-    CuMatMatMultMatrixI(A,B,C,3,2,3);
-    for(int i = 0; i < 9; i++){
+    CuMatAddMatrixI(A,B,C,3,2);
+    for(int i = 0; i < 6; i++){
         std::cout << C[i] << std::endl;
     }
-
-    std::cout << "With pinned memory:" << std::endl;
-    long * A_pinned;
-    long * B_pinned;
-    long * C_pinned;
-    CuMatCUDAAllocI(A, 6, A_pinned);
-    CuMatCUDAAllocI(B, 6, B_pinned);
-
-    CuMatMatMultMatrixI(A_pinned,B_pinned,C,3,2,3);
-    for(int i = 0; i < 9; i++){
-        std::cout << C[i] << std::endl;
-    }
-
-    CuMatCUDAFreeI(A_pinned);
-    CuMatCUDAFreeI(B_pinned);
-    CuMatCUDAFreeI(C_pinned);
 }
