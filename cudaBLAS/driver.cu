0019b24746e6a8c0c6697c975414a91f7e3874cd
Toby <toby.lawrance@gmail.com>
2021-04-08 15:33:03 +0100

    Tweaking to fix output size, gave default values to check for garbage

--- cudaBLAS/driver.cu
+++ cudaBLAS/driver.cu
@@ -4,9 +4,9 @@
 int main(){
     long A[6] = {1,2,7,3,4,1};
     long B[6] = {4,3,8,2,1,3};
-    long C[6];
+    long C[6] = {0,0,0,0,0,0};
 
-    CuMatAddMatrixI(A,B,C,6,6);
+    CuMatAddMatrixI(A,B,C,2,3);
     for(int i = 0; i < 6; i++){
         std::cout << C[i] << std::endl;
     }
