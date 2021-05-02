2a108887fccc0cb820bb4b95a305be8fd118e0df
Lloyd Conlin <lloyd.conlin@gmail.com>
2021-05-02 11:46:50 +0100

    Fix minor problem

--- src/parser/src/MatrixNode.cpp
+++ src/parser/src/MatrixNode.cpp
@@ -143,7 +143,6 @@ void AST::MatrixNode::semanticPass(Utils::IRContext* context) {
     Typing::MatrixType exprType;
     // Runs through all elements to check types and ranks
     for (auto& el : data) {
-        el->semanticPass(context);
         try {
             exprType = std::get<Typing::MatrixType>(*el->type);
         } catch (std::bad_cast b) {
