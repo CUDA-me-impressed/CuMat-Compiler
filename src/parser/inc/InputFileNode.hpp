6df9a89da8f9e9fae754ac9b36348c8ddfa4d6b7
Toby <toby.lawrance@gmail.com>
2021-05-05 15:47:05 +0100

    Base AST Node

---
+++ src/parser/inc/InputFileNode.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <llvm/IR/IRBuilder.h>
+
+#include <string>
+#include <vector>
+
+#include "ExprASTNode.hpp"
+
+namespace Analysis {
+class DimensionSymbolTable;
+}
+
+namespace AST {
+class InputFileNode : public ExprNode {
+   public:
+    std::string fileName{};
+    std::vector<uint> dimensions;
+
+    void semanticPass(Utils::IRContext* context) override;
+    void dimensionPass(Analysis::DimensionSymbolTable* nt) override;
+    llvm::Value* codeGen(Utils::IRContext* context) override;
+    [[nodiscard]] std::string toTree(const std::string& prefix, const std::string& childPrefix) const override;
+};
+}  // namespace AST
---
+++ src/parser/src/InputFileNode.cpp
@@ -0,0 +1,32 @@
+#include "FunctionExprNode.hpp"
+
+#include <map>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include "CodeGenUtils.hpp"
+#include "VariableNode.hpp"
+#include "TypeCheckingUtils.hpp"
+#include "DimensionsSymbolTable.hpp"
+#include "TreePrint.hpp"
+
+llvm::Value* AST::InputFileNode::codeGen(Utils::IRContext* context) {
+    //TODO: CALL C++ FUNCTION FOR THIS
+
+    return nullptr;
+
+}
+
+void AST::InputFileNode::semanticPass(Utils::IRContext* context) {
+    //TODO: Very little
+}
+
+std::string AST::InputFileNode::toTree(const std::string& prefix, const std::string& childPrefix) const {
+    //TODO: Optional
+}
+
+
+void AST::InputFileNode::dimensionPass(Analysis::DimensionSymbolTable* nt) {
+    //TODO: Just acknowledge?
+}
